5bca2550b839a913e143ee23e3458a484bcb5347
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-03-13 23:32:00 +0300

    13.3.2023

---
+++ 4_js_statement/readme.md
@@ -0,0 +1,12 @@
+let x, y, z ;
+x = 5 ; 
+y = 6 ;
+z =      x +     y ;
+document.getElementById("demo").innerHTML = "bu bir durumdur";
+
+function myFunction() {
+
+}
+my-function (yanlış)
+myFunction 
+firstName, lastName, (tercih edilir) 
---
+++ 5_js_varables/myScript.js
@@ -0,0 +1,118 @@
+// var x = 5;
+// var y = 6;
+// var z = x + y ;
+// console.log(z)
+
+
+// const price1 = 9 ;
+// const price2 = 6 ;
+// let total = price1 + price2 ;
+// console.log(total)
+
+// let x = 10;
+// x = x + 5 ;
+// console.log(x)
+
+// const pi = 3.14 ;
+// let person = "Mustafa";
+// let person2 = 'Selman';
+// console.log(person)
+
+// var carName1;
+// let carName2;
+// carName2 = "Volvo";
+// let carName3 = "Mercedes"
+// console.log(carName1)
+
+// let person = "Ahmet" , carName = "renoult" , price = 200 ;
+
+// console.log(price)
+// console.log(carName)
+
+// let carName = "volvo"
+// let carName tekrar tanımlanamaz
+
+// var carName = "volvo"
+// var carName = "reno"
+// console.log(carName)
+
+// // tanımlamalar önce yapılır
+// let adres = "istanbul"
+// adres = "ankara"
+
+// {
+//     var a1 = 2;
+// }
+
+// console.log(a1)
+
+//     let a1 = 3;
+// {
+//     let a1 = 4;
+// }
+// {
+//     let a1 = 4;
+//     console.log(a1)
+// }
+
+
+// carName = "audi"
+// var carName;
+// console.log(carName)
+
+// const PI = 3.14 ;
+// PI = 3 ;
+
+//diziler
+
+// const cars = ["mercedes","volvo","BMW"];
+// console.log(cars.length);
+// console.log(cars[0]);
+
+// cars.push("audi");
+// console.log(cars)
+
+// const cars = ["mercedes","volvo","BMW"];
+// cars = ["toyota","volvo","bmw"];
+
+
+//object
+// const car = {
+//     type: "Fiat",
+//     model: "500",
+//     color: "white",
+//     cost: 1000
+// }
+// console.log(car)
+// console.log(car.color)
+// car.color = "red"
+// console.log(car)
+// car.owner = "selman"
+// console.log(car)
+
+// let car = {
+// type:"Fiat", 
+// model:"500", 
+// color:"white"};
+
+// car = {
+// }; 
+// console.log(car)
+
+// eğer let ile yaparsak tüm veriyi istenmeyen şekilde değiştirebiliriz. o yüden tavsiye edilen const ile kullanmaktır(array,object,function)
+
+// const a = 1 ;
+// {
+//     const a = 2;
+// }
+
+// console.log(a)
+
+
+// bu ikisi de global değişkendir
+var isim = 1;
+isim2 = 2;
+
+// tek farkı değer atama zorunluluğudur.
+var isim3;
+isim4;
